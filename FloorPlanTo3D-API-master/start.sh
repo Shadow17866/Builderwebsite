141421f93a111c5123404d047e09024e2bd25111
Mandeep singh virdi <mandeepsinghvirdi0@gmail.com>
2026-01-02 18:42:06 +0530

    Switch to Gunicorn for production deployment with 5-minute timeout

--- FloorPlanTo3D-API-master/start.sh
+++ FloorPlanTo3D-API-master/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/nix/var/nix/profiles/default/lib
-exec /opt/venv/bin/python application.py
+exec /opt/venv/bin/gunicorn --bind 0.0.0.0:8080 --workers 1 --timeout 300 application:application
